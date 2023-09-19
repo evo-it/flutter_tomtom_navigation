11b51bfc54c2f4a130952f916de540bd29c01497
Matyas Kone <bikesvatera@gmail.com>
2023-09-19 14:19:08 +0200

    default project setup

--- flutter_tomtom_navigation/lib/flutter_tomtom_navigation.dart
+++ flutter_tomtom_navigation/lib/flutter_tomtom_navigation.dart
@@ -1,11 +1,4 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
-
-import 'flutter_tomtom_navigation_platform_interface.dart';
+import 'package:flutter_tomtom_navigation_platform_interface/flutter_tomtom_navigation_platform_interface%20copy.dart';
 
 class FlutterTomtomNavigation {
   Future<String?> getPlatformVersion() {
